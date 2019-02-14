0d182a0f1a232b7b5daae9e6e848b7dff36e9608
Vincent Chavez <vchavez92780@gmail.com>
2019-02-14 12:51:55 -0500

    Added application configuration files

---
+++ src/config.py
@@ -0,0 +1,28 @@
+"""Application configuration."""
+import os
+from dotenv import load_dotenv
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# load dotenv
+dotenv_path = os.path.join(ROOT_DIR, '.env')
+load_dotenv(dotenv_path)
+
+# Source IN dir
+IN_DIR = os.path.join(ROOT_DIR, 'in')
+
+# Set the settings
+config = {
+	'DB_SERVER': os.getenv('DB_HOST'),
+	'DB_NAME': os.getenv('DB_NAME'),
+	'DB_USER': os.getenv('DB_USER'),
+	'DB_PASSWORD': os.getenv('DB_PASSWORD'),
+	'DB_DRIVER': os.getenv('DB_DRIVER'),
+	'DB_TRUSTED_CONNECTION': os.getenv('DB_TRUSTED_CONNECTION') == 1,
+	'ROOT_DIR': ROOT_DIR,
+	'IN_DIR': IN_DIR
+}
+
+
+def get_config():
+	return config
